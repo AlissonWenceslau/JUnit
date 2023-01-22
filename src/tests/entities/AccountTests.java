58d6b027316ab266c6604fcdb4bd103737fe3c53
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-01-21 22:09:33 -0300

    Test:Do nothing when negative amount

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -23,4 +23,15 @@ public class AccountTests {
 		//Assert
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, expectedValue);
+		double amount = -200.0;
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
 }
